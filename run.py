dfe1615389552a78cef90702ce747dc58d46dee5
belonel <ksandr.lr@gmail.com>
2017-12-10 22:43:10 +0300

    with logs

--- run.py
+++ run.py
@@ -1,12 +1,16 @@
 import config
 import telebot
+import logging
 
 token = config.token
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start', 'find'])
 def main(message):
-    bot.send_message(message.chat.id, 'Привет!Введи слово и я выдам тебе цитату.')
+    try:
+        bot.send_message(message.chat.id, 'Привет!Введи слово и я выдам тебе цитату.')
+    except:
+        log.exception("Error!")
     
 if __name__ == '__main__':
      bot.polling(none_stop=True)
